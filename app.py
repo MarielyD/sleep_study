4eecfd43050fa549ae1f316da3b8117c8a657175
Mariely <mdlc0502@gmail.com>
2023-06-10 16:49:30 -0400

    second commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from components.layout import create_layout
 def main():
   app = Dash(external_stylesheets=[dbc.themes.LUX])
   server = app.server
-  app.title='Sleep study'
+  app.title='Sleep study '
   app.layout = create_layout(app)
   app.run_server(debug=True)
 
