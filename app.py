4e08b5ac72559593248b3c7d96a992567a10367c
Mariely <mdlc0502@gmail.com>
2023-06-15 22:13:47 -0400

    gunicorn commit

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from dash import Dash, html
 import dash_bootstrap_components as dbc
 from components.layout import create_layout
 
-def main():
-  app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
-  server = app.server
-  app.title='Sleep study '
-  app.layout = create_layout(app)
-  app.run_server(debug=True)
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+server = app.server
+app.title='Sleep study '
+app.layout = create_layout(app)
+
 
 if __name__ == '__main__':
-  main()
+  app.run_server(debug=True)
